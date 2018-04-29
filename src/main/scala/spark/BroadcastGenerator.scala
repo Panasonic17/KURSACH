3423c6d5f1faabbdc96f9ea6d2c8ae6523d276b2
Sawa <ifqyjuf17@gmail.com>
2018-04-29 21:04:24 +0300

    country and city

--- src/main/scala/spark/BroadcastGenerator.scala
+++ src/main/scala/spark/BroadcastGenerator.scala
@@ -4,12 +4,12 @@ import scala.io.Source
 
 class BroadcastGenerator {
 
-  def getIataCityName(pathToAiroportsDat: String): Any = {
+  def getIataCityName(pathToAiroportsDat: String):Map[String,String] = {
     val lines = Source.fromFile(pathToAiroportsDat).getLines.toList
     lines.map(row => row.split(",")).map(row => (row(4), row(2))).toMap
   }
 
-  def getIataCountryName(pathToAiroportsDat: String): Any = {
+  def getIataCountryName(pathToAiroportsDat: String):Map[String,String] = {
     val lines = Source.fromFile(pathToAiroportsDat).getLines.toList
     lines.map(row => row.split(",")).map(row => (row(4), row(3))).toMap
   }
